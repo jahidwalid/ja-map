e562b799ae82491c8ee7e091ec9acfbc8dd93a5b
jahed <developerjahed@gmail.com>
2021-12-26 22:05:30 +0600

    map testt

--- main.js
+++ main.js
@@ -19,5 +19,10 @@ omuk.startSession();
 
 const cube=x=> x*x*x; 
 console.log(cube(2))
+const [a, b] = [1,2,3,4,45,5]; console.log(a+b);
 
+const {x, y, z} = {x: 1, y1: 2, z: 3};
+
+console.log(x,y,z)
+const nums = [1,2,3,4,5]; let output = nums.filter(n => n%2); console.log(output);
 
